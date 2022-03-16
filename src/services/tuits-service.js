d3fbc93f722f76978b5e057d89cc4a490a39920e
shayan_jaleel <shayan.jaleel@gmail.com>
2022-03-16 09:25:42 -0700

    Change service urls to fetch from custom api.

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const TUITS_API = "https://cs5500-01-sp22.herokuapp.com/api/tuits";
-const USERS_API = "https://cs5500-01-sp22.herokuapp.com/api/users";
+const TUITS_API = "https://software-eng-a3-node-shayan.herokuapp.com/api/tuits";
+const USERS_API = "https://software-eng-a3-node-shayan.herokuapp.com/api/users";
 
 export const findAllTuits = () =>
     axios.get(TUITS_API)
