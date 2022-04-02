463f5a17f6f78c17a6078812424bff0b5d9f6b6e
shayan_jaleel <shayan.jaleel@gmail.com>
2022-04-02 18:51:06 -0400

    Update likes button icon.

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -15,11 +15,11 @@ const TuitStats = ({tuit, likeTuit = () => {}, dislikeTuit = () => {}}) => {
           <span onClick={() => likeTuit(tuit)}>
               {
                 tuit.stats && tuit.stats.likes && tuit.stats.likes > 0 &&
-                  <i className="fas fa-heart me-1" style={{color: 'red'}}></i>
+                  <i className="fas fa-solid fa-thumbs-up" style={{color: 'red'}}></i>
               }
               {
                 tuit.stats && tuit.stats.likes && tuit.stats.likes <= 0 &&
-                  <i className="far fa-heart me-1"></i>
+                  <i className="fas fa-solid fa-thumbs-up"></i>
               }
             {tuit.stats && tuit.stats.likes}
           </span>
